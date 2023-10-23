85c43f2000cc42d62a59a9fc368ba3d92a863c2b
Bella Melati <biabellamwdf@gmail.com>
2023-10-23 17:54:28 +0700

    Commit OCP2

---
+++ OCP2/DeluxeCinema.java
@@ -0,0 +1,9 @@
+public class DeluxeCinema extends Cinema{
+    public DeluxeCinema(Double price) {
+        this.price = price;
+        }
+        @Override
+        Double calculateAdminFee() {
+            return price * 12 / 100;
+            }
+}
